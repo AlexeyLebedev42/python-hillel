3636d2522322b488046b5358bb8cf94f721171a3
AlexeyLebedev42 <bestempire68@gmail.com>
2023-12-06 14:08:22 +0200

    HW23

---
+++ HW23/HW23.py
@@ -0,0 +1,15 @@
+from inspect import isgenerator
+def pow(x):
+    return x ** 2
+
+def some_gen(begin, end, func):
+    for i in range(end):
+        if i < end:
+            i += 1
+        yield begin
+        begin = func(begin)
+
+gen = some_gen(2, 4, pow)
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')
