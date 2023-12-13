c85d0eedc747c89986004ff6583c1d349d035d01
AlexeyLebedev42 <bestempire68@gmail.com>
2023-12-13 11:09:10 +0200

    Homework24

---
+++ HW24/HW24.py
@@ -0,0 +1,19 @@
+def prime_generator(end):
+    count = 0
+    for i in range(2, end + 1):
+        for j in range(1, i + 1):
+            if i % j == 0:
+                count += 1
+        if count <= 2:
+            yield i
+        count = 0
+
+
+from inspect import isgenerator
+
+gen = prime_generator(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
+assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
+assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
+print('Ok')
